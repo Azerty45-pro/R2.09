6ce8a454a9eadda95a67240551977fd0aaba8609
e2505535 <bilalfalana.pro@gmail.com>
2026-05-06 16:55:56 +0200

    2.09

--- firstproject/myfirstapp/urls.py
+++ firstproject/myfirstapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('index/', views.index),
     path('formulaire/', views.formulaire),
     path('bonjour/', views.bonjour),
--- firstproject/myfirstapp/views.py
+++ firstproject/myfirstapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'myfirstapp/home.html')
+
 def index(request):
     return render(request, 'myfirstapp/index.html')
 
